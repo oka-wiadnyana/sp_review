d9b5ee44e6f0c0a252ab136c5a8975f6024f8b8d
Oka Wiadnyana <onsdee86@gmail.com>
2024-03-01 09:40:18 +0800

    feat: change title

--- resources/views/livewire/create-review.blade.php
+++ resources/views/livewire/create-review.blade.php
@@ -1,9 +1,12 @@
 <div class=" h-screen flex justify-center items-center">
     <div >
-        <div class="flex justify-center mb-3">
+        <div class="flex flex-col text-center justify-center mb-3">
 
             <img src="{{ asset('img/logo negara.png') }}" alt="" class="object-scale-down h-24 w-54 me-2 ">
-            <span class="text-4xl hidden md:block font-sans font-bold my-auto">Review Standar Pelayanan</span>
+            <span class="text-4xl hidden md:block font-sans font-bold my-auto">RESPATI</span>
+            
+            <span class="text-2xl hidden md:block font-sans font-bold my-auto">Review Standar Pelayanan berbasis TI</span>
+            
         </div>
         <div class="  ">
             <form wire:submit="create" class="w-full">
